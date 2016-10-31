ec4f6e65df721a2d2759c232d7e4fd6b929d15e9
Maciej Walaszczyk <walaszczykm@gmail.com>
2016-10-31 02:04:32 +0100

    player model and player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+    [SerializeField]
+    private float runFactor = 2.0f;
+    [SerializeField]
+    private float rotateSpeed = 1.0f;
+    [SerializeField]
+    private Transform character;
+    [SerializeField]
+    private Transform cameraPoint;
+    private CharacterController controller;
+
+    void Awake()
+    {
+        controller = character.GetComponent<CharacterController>();
+    }
+	
+	void FixedUpdate()
+    {
+        Jump();
+        Move();
+        Rotate();
+        CameraUpdate();
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+
+    private void Move()
+    {
+        float speed = moveSpeed * (Input.GetKey(KeyCode.LeftShift) 
+            ? runFactor : 1.0f);
+        Vector3 input = character.right * Input.GetAxisRaw("Horizontal") + 
+            character.forward * Input.GetAxisRaw("Vertical");
+        input = input.normalized;
+        controller.SimpleMove(input * speed);
+    }
+
+    private void Rotate()
+    {
+        Vector3 input = new Vector3(0, Input.GetAxisRaw("Mouse X"), 0).normalized;
+        character.Rotate(input * rotateSpeed);
+    }
+
+    private void CameraUpdate()
+    {
+        cameraPoint.eulerAngles = character.eulerAngles;
+        cameraPoint.position = Vector3.Lerp(cameraPoint.position, character.position, Time.deltaTime * 5.0f);
+    }
+}
